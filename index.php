dc40f2f86341610f8b538863521d766ea7966fd5
JakubVavru <JakubVavru@users.noreply.github.com>
2020-03-03 08:57:08 +0100

    Podmínka věku
    Prodej alkoholických nápoju :)

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$age = 20;
+
+if ($age >= 18) {
+   $answer = "Tak si dej chlapče.";
+} else {
+    $answer = "Tak to teda ne. Alkoholické nápoje maldším 18-ti let neprodáváme."; 
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+  <h3> <?= $answer; ?> </h3>
+
+</body>
+</html>
